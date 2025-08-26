ec034898920eff89919d495effec93981d73db9b
Mark <brandonnarciso24@gmail.com>
2025-08-26 02:34:33 -0700

    chore: Build basic JWT

--- server/src/routes/login.ts
+++ server/src/routes/login.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import type { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 
 const loginRouter = Router();
 
@@ -9,8 +10,27 @@ loginRouter.get("/", (req: Request, res: Response) => {
 
 // Implement a
 // I don't even know what I am trying to say up there LOL
-loginRouter.post("/", (req: Request, res: Response) => {
-  res.status(201).send(req.body);
+loginRouter.post("/", (req: Request, res: Response, next) => {
+  //! Mock User later update this to get the user and authenticate the user
+  const user = {
+    id: 1,
+    username: "Khaspper",
+    email: "abc@gmail.com",
+  };
+  //! Mock User later update this to get the user and authenticate the user
+  console.log("Hiiii");
+  jwt.sign(
+    {
+      user: user,
+    },
+    "secretKey",
+    { expiresIn: "1d" },
+    (err, token) => {
+      if (err || !token)
+        return next(err ?? new Error("JWT FAILED TOKEN CREATION FAILED"));
+      res.json({ token });
+    }
+  );
 });
 
 export default loginRouter;
