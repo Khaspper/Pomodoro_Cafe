309db38dc1db810ba2d290964d6ab5e61d28c0d1
Mark <brandonnarciso24@gmail.com>
2025-08-26 19:59:26 -0700

    chore: create a seed file to populate db

---
+++ server/src/db/seed.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "../../generated/prisma";
+
+const prisma = new PrismaClient();
+
+async function seed() {
+  await prisma.user.create({
+    data: {
+      username: "khaspper",
+      password: "qwerty",
+      email: "mark@example.com",
+    },
+  });
+}
+
+seed()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
