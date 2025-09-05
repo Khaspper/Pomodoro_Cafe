f71be06c54dda06a8d9fd66b7d03b17b884c5709
Mark <brandonnarciso24@gmail.com>
2025-09-05 04:34:36 -0700

    feat(API): Find an API that retrieves cafes' locs

---
+++ server/src/controllers/getCafes.ts
@@ -0,0 +1,54 @@
+export async function getCafesFromOverPass() {
+  const query = `
+[out:json][timeout:180];
+
+(
+  nwr["amenity"="cafe"](35.95,-115.47,36.36,-114.92);
+  nwr["shop"="coffee"](35.95,-115.47,36.36,-114.92);
+  nwr["shop"="tea"](35.95,-115.47,36.36,-114.92);
+)->.all;
+
+area["aeroway"="aerodrome"]["name"="Harry Reid International Airport"]->.airport;
+(
+  nwr["amenity"="cafe"](area.airport);
+  nwr["shop"="coffee"](area.airport);
+  nwr["shop"="tea"](area.airport);
+)->.airportHits;
+
+(
+  way["aeroway"~"^(runway|taxiway|apron)$"](area.airport);
+)->.aerowayWays;
+
+nwr(around.aerowayWays:250)["amenity"="cafe"]->.airportBuf1;
+nwr(around.aerowayWays:250)["shop"="coffee"]->.airportBuf2;
+nwr(around.aerowayWays:250)["shop"="tea"]->.airportBuf3;
+
+(.airportHits; .airportBuf1; .airportBuf2; .airportBuf3;)->.airportAll;
+
+(
+  nwr["amenity"="cafe"](36.080,-115.185,36.145,-115.145);
+  nwr["shop"="coffee"](36.080,-115.185,36.145,-115.145);
+  nwr["shop"="tea"](36.080,-115.185,36.145,-115.145);
+)->.stripHits;
+
+(.all; - .airportAll;)->.noAirport;
+(.noAirport; - .stripHits;)->.filtered;
+
+(.filtered;);
+out center tags;
+
+
+`;
+
+  const res = await fetch("https://overpass-api.de/api/interpreter", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: "data=" + encodeURIComponent(query),
+  });
+
+  const data = await res.json();
+  console.log("data");
+  console.log(data);
+}
+
+getCafesFromOverPass();
