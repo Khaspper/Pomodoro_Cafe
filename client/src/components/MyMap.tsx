5b0fe1f79e880bd5bf40f4cb9d84920a686cfc16
Mark <brandonnarciso24@gmail.com>
2025-09-07 04:27:10 -0700

    feat(map): Add markers to the map

--- client/src/components/MyMap.tsx
+++ client/src/components/MyMap.tsx
@@ -1,22 +1,57 @@
-import { Map } from "@vis.gl/react-maplibre";
+import { Map, Marker } from "@vis.gl/react-maplibre";
+import { useState, useEffect } from "react";
 import "maplibre-gl/dist/maplibre-gl.css";
 
 export default function MyMap() {
+  const [cafes, setCafes] = useState([]);
+  console.log(cafes);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("http://localhost:3000/");
+      if (!response.ok) {
+        throw new Error("Failed to get all cafes.");
+      }
+      const cafes = await response.json();
+      setCafes(cafes);
+    }
+    fetchData();
+  }, []);
+
   const lasVegasBounds: [[number, number], [number, number]] = [
-    [-115.413944, 35.884647],
-    [-114.681238, 36.411675],
+    [-115.486398, 35.916672],
+    [-114.907841, 36.411699],
   ];
 
+  if (cafes.length === 0) {
+    return <h1 className="text-5xl text-black">Loading</h1>;
+  }
+
   return (
-    <Map
-      initialViewState={{
-        longitude: -115.1398,
-        latitude: 36.1699,
-        zoom: 11,
-      }}
-      style={{ width: "100vw", height: "100vh" }}
-      mapStyle="https://tiles.openfreemap.org/styles/liberty"
-      maxBounds={lasVegasBounds}
-    />
+    <div className="relative">
+      <Map
+        initialViewState={{
+          longitude: -115.1398,
+          latitude: 36.1699,
+          zoom: 5,
+        }}
+        style={{ width: "100vw", height: "100vh" }}
+        mapStyle="https://tiles.openfreemap.org/styles/liberty"
+        maxBounds={lasVegasBounds}
+      >
+        {cafes.map((cafe: { id: number; lon: number; lat: number }) => (
+          <Marker
+            key={cafe.id}
+            longitude={cafe.lon}
+            latitude={cafe.lat}
+            anchor="bottom"
+            className="cursor-pointer"
+          >
+            <div className="text-xl">☕</div>
+          </Marker>
+        ))}
+      </Map>
+      {/* <h1 className="absolute top-0 right-0 z-2">hi</h1> */}
+    </div>
   );
 }
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,25 +1,8 @@
 import MyMap from "../components/MyMap";
-import { useState, useEffect } from "react";
 
 type THomeProps = { children: React.ReactNode };
 
 export default function Home({ children }: THomeProps) {
-  const [cafes, setCafes] = useState([]);
-
-  console.log(cafes[0]);
-
-  useEffect(() => {
-    async function fetchData() {
-      const response = await fetch("http://localhost:3000/");
-      if (!response.ok) {
-        throw new Error("Failed to get all cafes.");
-      }
-      const cafes = await response.json();
-      setCafes(cafes);
-    }
-    fetchData();
-  }, []);
-
   return (
     <div className="relative">
       <div className="absolute z-1 w-screen">{children}</div>
