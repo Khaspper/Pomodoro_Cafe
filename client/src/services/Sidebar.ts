cfa23b149de185fc246b16ee3ecbe2b91967e704
Mark <brandonnarciso24@gmail.com>
2025-09-09 21:19:20 -0700

    chore: build a fetch function to send to backend

---
+++ client/src/services/Sidebar.ts
@@ -0,0 +1,13 @@
+export async function addSong(spotifyLink: string, cafeID: number) {
+  const response = await fetch(
+    `http://localhost:3000/cafe/${cafeID}/inputs/song`,
+    {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+      body: JSON.stringify({ spotifyLink, cafeID }),
+    }
+  );
+  console.log("response");
+  console.log(response);
+}
