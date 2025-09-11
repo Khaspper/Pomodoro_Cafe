9ccf660cbd2a427da73b512d20a903e395624ed8
Mark <brandonnarciso24@gmail.com>
2025-09-10 19:30:23 -0700

    fix(UI): hover over Data wrong

--- client/src/components/sidebar/ReviewCafe.tsx
+++ client/src/components/sidebar/ReviewCafe.tsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom";
 
 export default function ReviewCafe({ selectedCafe }: { selectedCafe: TCafe }) {
   return (
-    <div className="text-[#fbe3ad] bg-[#4c6850] p-2 rounded-lg mt-2 mb-4 text-center">
-      <Link to={`/review/${selectedCafe.id}`} className="text-2xl">
+    <Link to={`/review/${selectedCafe.id}`} className="text-2xl">
+      <div className="text-[#fbe3ad] bg-[#4c6850] p-2 rounded-lg mt-2 mb-4 text-center">
         Data wrong? Improve it here!
-      </Link>
-    </div>
+      </div>
+    </Link>
   );
 }
