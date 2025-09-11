968f71fb12fa1e6c3dc4fd281540d1a839dd4417
Mark <brandonnarciso24@gmail.com>
2025-09-10 19:34:49 -0700

    fix(Sign up form): Delete Extra Div

--- client/src/components/SignupForm.tsx
+++ client/src/components/SignupForm.tsx
@@ -51,99 +51,103 @@ export default function SignupForm() {
   }
 
   return (
-    <div className="bg-[#03304f] p-5 text-[#b1372c] rounded-2xl w-[300px] md:w-[400px] mt-[-100px]">
-      <form className="flex flex-col gap-2 md:gap-4" onSubmit={handleSubmit}>
-        <h1 className="text-4xl md:text-5xl text-center font-bold">Sign-up</h1>
-        <div className="flex flex-col">
-          <label className="font-bold md:text-xl" htmlFor="username">
-            Username
-          </label>
-          <input
-            className="border-2 rounded-xl px-2 md:px-4 py-1 border-[#fbe3ad] font-bold md:text-xl outline-none"
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            name="username"
-            id="username"
-            required
-          />
-          <p className="text-[#df9f3f]">
-            {errors.username ? errors.username : ""}
+    <div className="bg-[#1a1a1a] min-h-screen flex justify-center items-center">
+      <div className="bg-[#03304f] p-5 text-[#b1372c] rounded-2xl w-[300px] md:w-[400px] mt-[-100px]">
+        <form className="flex flex-col gap-2 md:gap-4" onSubmit={handleSubmit}>
+          <h1 className="text-4xl md:text-5xl text-center font-bold">
+            Sign-up
+          </h1>
+          <div className="flex flex-col">
+            <label className="font-bold md:text-xl" htmlFor="username">
+              Username
+            </label>
+            <input
+              className="border-2 rounded-xl px-2 md:px-4 py-1 border-[#fbe3ad] font-bold md:text-xl outline-none"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              name="username"
+              id="username"
+              required
+            />
+            <p className="text-[#df9f3f]">
+              {errors.username ? errors.username : ""}
+            </p>
+          </div>
+          <div className="flex flex-col">
+            <label className="font-bold md:text-xl" htmlFor="email">
+              Email
+            </label>
+            <input
+              className="border-2 rounded-xl px-2 md:px-4 py-1 border-[#fbe3ad] font-bold md:text-xl outline-none"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              name="email"
+              id="email"
+              required
+            />
+            <p className="text-[#df9f3f]">{errors.email ? errors.email : ""}</p>
+          </div>
+          <div className="flex flex-col">
+            <label className="font-bold md:text-xl" htmlFor="password">
+              Password
+            </label>
+            <input
+              className="border-2 rounded-xl px-2 md:px-4 py-1 border-[#fbe3ad] font-bold md:text-xl outline-none"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              name="password"
+              id="password"
+              required
+            />
+            <p className="text-[#df9f3f]">
+              {errors.password ? errors.password : ""}
+            </p>
+          </div>
+          <div className="flex flex-col">
+            <label className="font-bold md:text-xl" htmlFor="confirmPassword">
+              Confirm Password
+            </label>
+            <input
+              className="border-2 rounded-xl px-2 md:px-4 py-1 border-[#fbe3ad] font-bold md:text-xl outline-none"
+              type="password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              name="confirmPassword"
+              id="confirmPassword"
+              required
+            />
+            <p className="text-[#df9f3f]">
+              {errors.confirmPassword ? errors.confirmPassword : ""}
+            </p>
+          </div>
+          {/* Add if loading grey out the button */}
+          <div className="flex justify-between">
+            <button
+              className="cursor-pointer font-bold bg-[#b1372c] py-2 rounded-2xl md:text-xl w-[40%] md:w-[35%] text-[#1a1a1a]"
+              type="submit"
+              onClick={handleClick}
+            >
+              Go Home
+            </button>
+            <button
+              className="cursor-pointer font-bold bg-[#b1372c] py-2 rounded-2xl md:text-xl w-[40%] md:w-[35%] text-[#1a1a1a]"
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? "Submitting..." : "Sign-up"}
+            </button>
+          </div>
+          <p className="text-md font-bold text-[#fbe3ad]">
+            Have an account?{" "}
+            <Link className="underline" to={"/login"}>
+              Login!
+            </Link>
           </p>
-        </div>
-        <div className="flex flex-col">
-          <label className="font-bold md:text-xl" htmlFor="email">
-            Email
-          </label>
-          <input
-            className="border-2 rounded-xl px-2 md:px-4 py-1 border-[#fbe3ad] font-bold md:text-xl outline-none"
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            name="email"
-            id="email"
-            required
-          />
-          <p className="text-[#df9f3f]">{errors.email ? errors.email : ""}</p>
-        </div>
-        <div className="flex flex-col">
-          <label className="font-bold md:text-xl" htmlFor="password">
-            Password
-          </label>
-          <input
-            className="border-2 rounded-xl px-2 md:px-4 py-1 border-[#fbe3ad] font-bold md:text-xl outline-none"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            name="password"
-            id="password"
-            required
-          />
-          <p className="text-[#df9f3f]">
-            {errors.password ? errors.password : ""}
-          </p>
-        </div>
-        <div className="flex flex-col">
-          <label className="font-bold md:text-xl" htmlFor="confirmPassword">
-            Confirm Password
-          </label>
-          <input
-            className="border-2 rounded-xl px-2 md:px-4 py-1 border-[#fbe3ad] font-bold md:text-xl outline-none"
-            type="password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-            name="confirmPassword"
-            id="confirmPassword"
-            required
-          />
-          <p className="text-[#df9f3f]">
-            {errors.confirmPassword ? errors.confirmPassword : ""}
-          </p>
-        </div>
-        {/* Add if loading grey out the button */}
-        <div className="flex justify-between">
-          <button
-            className="cursor-pointer font-bold bg-[#b1372c] py-2 rounded-2xl md:text-xl w-[40%] md:w-[35%] text-[#1a1a1a]"
-            type="submit"
-            onClick={handleClick}
-          >
-            Go Home
-          </button>
-          <button
-            className="cursor-pointer font-bold bg-[#b1372c] py-2 rounded-2xl md:text-xl w-[40%] md:w-[35%] text-[#1a1a1a]"
-            type="submit"
-            disabled={loading}
-          >
-            {loading ? "Submitting..." : "Sign-up"}
-          </button>
-        </div>
-        <p className="text-md font-bold text-[#fbe3ad]">
-          Have an account?{" "}
-          <Link className="underline" to={"/login"}>
-            Login!
-          </Link>
-        </p>
-      </form>
+        </form>
+      </div>
     </div>
   );
 }
--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,9 +1,5 @@
 import SignupForm from "../components/SignupForm";
 
 export default function Signup() {
-  return (
-    <div className="bg-[#1a1a1a] min-h-screen flex justify-center items-center">
-      <SignupForm />
-    </div>
-  );
+  return <SignupForm />;
 }
