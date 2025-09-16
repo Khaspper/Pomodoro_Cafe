1a3cb78e14bdb8cde7ff4913aabe7761f9a42598
Mark <brandonnarciso24@gmail.com>
2025-09-15 22:29:04 -0700

    set up api route for updating users info

--- server/src/routes/Account.ts
+++ server/src/routes/Account.ts
@@ -4,7 +4,16 @@ import { isAuthenticated, logout } from "../controllers/accountController";
 const accountRouter = Router();
 
 accountRouter.get("/", isAuthenticated, (req, res) => {
-  res.status(200).json({ message: "Updated User..." });
+  res.status(200).json(req.user);
+});
+
+accountRouter.post("/", isAuthenticated, (req, res) => {
+  const username = req.body.username;
+  const email = req.body.email;
+  const password = req.body.password;
+  console.log(username);
+  console.log(email);
+  console.log(password);
 });
 
 accountRouter.post("/logout", isAuthenticated, logout);
